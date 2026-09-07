14e7d15f7c67a7ec09f14607f5b899f369c6f2b4
joackagui <joaquinaguilerasalinas1@gmail.com>
2026-09-07 17:25:04 -0400

    Files re arrangement

---
+++ Class_09/07/example.tsx
@@ -0,0 +1,8 @@
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
